884e1a275a821fe4c7a4e5759d5807e0004e1739
ber01 <ber01@naver.com>
2018-07-09 13:54:46 +0900

    피보나치 수5

---
+++ Java/_10870.java
@@ -0,0 +1,31 @@
+package Algorithm.BaekJoon;
+
+import java.util.*;
+
+public class _10870 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(fibo(sc.nextInt()));
+
+        sc.close();
+    }
+
+    public static long[] dp = new long[91];
+    private static long fibo(int n){
+
+        if(n <= 1){
+            return n;
+        } else{
+
+            if(dp[n] > 0){
+                return dp[n];
+            }
+
+            dp[n] = fibo(n-1) + fibo(n-2);
+            return dp[n];
+        }
+    }
+}
--- README.md
+++ README.md
@@ -56,3 +56,4 @@
 - 2747 : 피보나치 수 (https://www.acmicpc.net/problem/2747)
 - 2748 : 피보나치 수2 (https://www.acmicpc.net/problem/2748)
 - 10826 : 피보나치 수4 (https://www.acmicpc.net/problem/10826)
+- 10870 : 피보나치 수5 (https://www.acmicpc.net/problem/10870)
